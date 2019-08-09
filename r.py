19299eb91a2ff8cc06cf3dc4042187ce3421bd65
Leo Wu <leo0622@hotmail.com.tw>
2019-08-09 23:26:33 +0800

    add cound

--- r.py
+++ r.py
@@ -1,13 +1,20 @@
 import random
-
-r = random.randint(1, 100)
+start = input('請輸入起始值:')
+end = input('請輸入結束值')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
+count = 0
 while True:
+	count += 1 
 	num = input('請猜數字: ')
 	num = int(num)
 	if num == r:
 		print('你猜中了')
+		print('這是你猜的第', count, '次')
 		break
 	elif num > r:
 		print('比答案大')
 	elif num < r:
 		print('比答案小')
+	print('這是你猜的第', count, '次')
